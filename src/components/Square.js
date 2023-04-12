4d2790cc4409e14b2a41c47388a2187ae75468b5
AnnikaSonnek <annika.sonnek@gmail.com>
2023-04-12 11:35:37 +0200

    got it to work

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Player } from 'components/Player'
+import { game } from 'reducers/game'
 
 export const Square = ({ value, index }) => {
+  const dispatch = useDispatch()
   const handleClick = () => {
+    dispatch(game.actions.captureSquare({ index: index }))
+
     // TODO send the captureSquare action
   }
 
